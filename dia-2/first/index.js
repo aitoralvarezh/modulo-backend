6c1c8b59ce0ff0fca0d82830965a0b630e54cd01
aitoralvarezh <hermida703@gmail.com>
2020-10-29 19:31:06 +0100

    actualizacion del dia 1 y correccion del dia 2

---
+++ dia-2/first/index.js
@@ -0,0 +1,15 @@
+const faker = require ('faker');
+
+console.log(faker.address.city());
+
+console.log(faker.image.city());
+console.log('cambio autoejecutable con nodemon');
+
+//usar ./node_modules/.bin/nodemon'
+const lodash = require ('lodash')
+
+const myarr = [1,2,3,4,5,6,6,7,8,8,9,9];
+const myarr2 = [28, 29]
+const myarrNoDups = lodash.uniq(myarr);
+
+console.log(myarrNoDups);
